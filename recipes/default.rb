bc6528b67b4d40c73f4084f5c68e3d3b119dbca0
anb <anb@zuehlke.com>
2012-05-22 18:08:43 +0200

    Added user

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,3 +12,18 @@ postgresql_database 'camp2012db' do
   connection ({:host => "127.0.0.1", :port => 5432, :username => 'postgres', :password => node['postgresql']['password']['postgres']})
   action :create
 end
+
+postgresql_database_user 'zeg' do
+	connection ({:host => "127.0.0.1", :port => 5432, :username => 'postgres', :password => node['postgresql']['password']['postgres']})
+	password 'geheim!'
+	action :create
+end;
+
+postgresql_database_user 'zeg' do
+	connection ({:host => "127.0.0.1", :port => 5432, :username => 'postgres', :password => node['postgresql']['password']['postgres']})
+	password 'geheim!'
+    database_name 'camp2012db'
+#    host '%'
+#    privileges [:select,:update,:insert]
+	action :grant
+end;
