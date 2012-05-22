b5cce36d8893474dd35f5e9f9e6a572b33c9e5f3
anb <anb@zuehlke.com>
2012-05-22 17:16:46 +0200

    Added dummy db

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# create a postgresql database
+postgresql_database 'camp2012DB' do
+  connection ({:host => "127.0.0.1", :port => 5432, :username => 'postgres', :password => node['postgresql']['password']['postgres']})
+  action :create
+end
